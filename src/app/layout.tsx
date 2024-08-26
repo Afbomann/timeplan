b3e4cbe3f339cbec235a46d8da1dfbd40ff825a1
Logisteaf <178702842+Afbomann@users.noreply.github.com>
2024-08-26 21:12:02 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Timeplan",
+  title: "Timeplan - IT",
   description: "Timeplan for IT, ingen visma innlogging😁",
 };
 
